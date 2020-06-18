d20ddd06d953c74bf8d5306f9ddd41eb07f9345b
Bryce Whittington <brycew30@gmail.com>
2020-06-18 18:01:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,9 @@ def my_collect(array)
   counter = 0
   collection_array = []
   while counter < array.length
-   yield(array[counter])
+   collection_array << yield(array[counter])
    counter += 1
   end
-  array
+  collection_array
 end
 
