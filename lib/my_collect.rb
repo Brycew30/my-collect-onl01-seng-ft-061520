1f3eabdc30c11592ff74f9d9a3489ea34e6150ef
Bryce Whittington <brycew30@gmail.com>
2020-06-18 17:48:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,10 @@
 array = ["a","b","c","d"]
 def my_collect(array) do |items|
-  items.upcase
+  counter = 0
+  while counter < array.length
+   yield(array[counter])
+   counter += 1
+  end
+  array
 end
 
